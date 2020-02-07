a6bfb2e9f2a270a508e8b2477c6fa771517da79b
Tommy Vo <s531823@nwmissouri.edu>
2020-02-07 14:23:30 -0600

    Implemented error checking for input

--- logic.js
+++ logic.js
@@ -1,13 +1,40 @@
 function calculate() {
-    var totalAmount = document.getElementById("totalInput").value;
-    var rateAmount = document.getElementById("rateInput").value * 0.01; //convert whole number to percentage
+    document.getElementById("errorMSG").innerHTML = ""; //clear error message
     
-    var tipAmount = totalAmount * rateAmount;
-    var taxAmount = totalAmount * 0.055;
-    var grandTotalAmount = parseFloat(totalAmount) + parseFloat(tipAmount) + parseFloat(taxAmount); //parseFloat converts the strings to floats
+    //parseFloat converts the strings to floats
+    var totalAmount = parseFloat(document.getElementById("totalInput").value);
+    var rateAmount = parseFloat(document.getElementById("rateInput").value);
+    
+    if(isNaN(totalAmount)) {
+        document.getElementById("errorMSG").innerHTML = "Put in a real dollar amount, ya dingus."
+    }
+    if(isNaN(rateAmount)) { //if rate input is not a valid input, set it to 0.0
+        rateAmount = 0.0;
+    }
+
+    var tipAmount = parseFloat(totalAmount) * (parseFloat(rateAmount) * 0.01);
+    var taxAmount = parseFloat(totalAmount) * 0.055;
+    var grandTotalAmount = parseFloat(totalAmount) + parseFloat(tipAmount) + parseFloat(taxAmount);
     
     //toFixed() rounds the values to the ()th decimal
-    document.getElementById("tipInfo").innerHTML = "$" + tipAmount.toFixed(2);
-    document.getElementById("taxInfo").innerHTML = "$" + taxAmount.toFixed(2);
-    document.getElementById("grandTotalInfo").innerHTML = "$" + grandTotalAmount.toFixed(2);
+    if(isNaN(tipAmount)) {
+        document.getElementById("tipInfo").innerHTML = "$0.00";
+    }
+    else {
+        document.getElementById("tipInfo").innerHTML = "$" + tipAmount.toFixed(2);
+    }
+    
+    if(isNaN(taxAmount)) {
+        document.getElementById("taxInfo").innerHTML = "$0.00";
+    }
+    else {
+        document.getElementById("taxInfo").innerHTML = "$" + taxAmount.toFixed(2);
+    }
+    
+    if(isNaN(grandTotalAmount)) {
+        document.getElementById("grandTotalInfo").innerHTML = "$0.00";
+    }
+    else {
+        document.getElementById("grandTotalInfo").innerHTML = "$" + grandTotalAmount.toFixed(2);
+    }
 }
